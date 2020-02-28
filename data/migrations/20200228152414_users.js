939b1a481396e2b63f8cff1e03bc5a2a86fcbfe5
donutwizard666 <thebeardedwarrior@icloud.com>
2020-02-28 15:41:27 -0800

    made user and entry tables

---
+++ data/migrations/20200228152414_users.js
@@ -0,0 +1,20 @@
+exports.up = function(knex) {
+  knex.schema.createTable('users', tbl => {
+    tbl.increments();
+    tbl.string('firstname', 30).notNullable();
+    tbl.string('lastname', 30).notNullable();
+    tbl
+      .string('username', 30)
+      .notNullable()
+      .unique();
+    tbl
+      .string('email', 50)
+      .notNullable()
+      .unique();
+    tbl.string('password', 128).notNullable();
+  })
+};
+
+exports.down = function(knex) {
+  knex.schema.dropTableIfExists('users');
+};
---
+++ data/migrations/20200228153214_entries.js
@@ -0,0 +1,20 @@
+exports.up = function(knex) {
+  knex.schema.createTable('entries', tbl => {
+    tbl.increments();
+    tbl.string('title', 250).notNullable();
+    tbl.text('text').notNullable();
+    tbl.datetime('created_at', { precision: 6}).defaultTo(knex.fn.now(6));
+    tbl
+      .integer('user_id')
+      .notNullable()
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  })
+};
+
+exports.down = function(knex) {
+  knex.schema.raw('DROP TABLE if exists entries cascade');
+};
